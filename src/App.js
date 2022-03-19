fa22c326a0576f0a71fff1d501c095771f2fe927
Killaskt <thegimreper@gmail.com>
2022-03-19 16:42:07 -0400

    redux toolkit playgorund bb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counter/counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { count } = useSelector((state) => state.counter);
+    const [yolo, setYolo] = useState(0);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <h1> {count} people are learning react</h1>
+                <div>
+                    <button onClick={() => dispatch(increment())}>
+                        INCREMENT
+                    </button>
+                    <button onClick={() => dispatch(decrement())}>
+                        DECREMENT
+                    </button>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
